252fa0429c5eda42bb6afdb7bbb14c231e79651b
Nate Magera <nmagera07@gmail.com>
2019-10-30 18:44:03 -0400

    starting the project

--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+
+        p = f"Room Name:   {self.name}\n"
+        return p
+
+    def __repr__(self):
+        return f"Name({repr(self.name)})"
